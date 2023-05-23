f2f54be6436df051e03b2f916c77cc8212777059
Adam Stasiak <astasiak93@gmail.com>
2023-05-23 14:42:39 +0200

    cleanup

--- cypress_block/cypress/e2e/interfaceBasedPageObject/interfaceBasedPageObject.cy.ts
+++
@@ -1,9 +0,0 @@
-import HomePage from 'cypress/pages/elements/interfacePageObject/homePageInterfaceVersion';
-
-describe('Interface Page Object', () => {
-  let homePage: HomePage = new HomePage();
-  it('Header should be visible.', () => {
-    homePage.visit();
-    homePage.getHeader().should('be.visible');
-  });
-});
--- cypress_block/cypress/pages/elements/advancedPageObject/listingPage.ts
+++
@@ -1,8 +0,0 @@
-import Page from './page';
-
-class ListingPage extends Page {
-  constructor() {
-    super();
-  }
-}
-export default ListingPage;
